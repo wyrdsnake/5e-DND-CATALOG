1ea82005abedf232a6df5783dfdb79467f80c9e8
obey-me <ethandladd@gmail.com>
2020-05-04 15:55:57 -0400

    Update utils.py

--- utils.py
+++ utils.py
@@ -2,5 +2,5 @@ import discord
 
 # creates a standard message
 def std_embed():
-    embed=discord.Embed(title="**Calligula's Compendium**", color=0xEA4246, description="@Ethan, probably put a better desc. of what the bot is supposed to do here.")
+    embed=discord.Embed(title="**Calligula's Compendium**", color=0xEA4246, description="A bot that compiles races, classes, equipment, and more!")
     return embed
